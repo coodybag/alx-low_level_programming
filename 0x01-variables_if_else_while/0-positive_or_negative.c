a0eb528c07f6285854194444a1f0162e96bed1f2
Adegbenga Ogundeko <oadegbenga@gmail.com>
2022-07-02 07:12:35 -0700

    Task 00: Positve, Zero or Negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return : Always 0 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int n;
-
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
@@ -16,8 +16,6 @@ printf("%d is positive\n", n);
 if (n == 0)
 printf("%d is zero\n", n);
 if (n < 0)
-{
 printf("%d is negative\n", n);
-}
 return (0);
 }

b4e73cb9bdf7f739399180d93e262e7e90957355
Adegbenga Ogundeko <oadegbenga@gmail.com>
2022-07-02 15:06:22 -0700

    Task 09: Combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0
+*/
+int main(void)
+{
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}

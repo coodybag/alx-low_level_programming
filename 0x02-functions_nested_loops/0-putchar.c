2fd459ac1616c81b2ee9f3d1b086789f06148bae
Adegbenga Ogundeko <oadegbenga@gmail.com>
2022-07-05 17:58:16 -0700

    Task 00: Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+/**
+*main -  writes the character _putchar to stdout
+*Return: Always 0 Success
+*/
+int main(void)
+{
+char word[8] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+_putchar(word[i]);
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This project is aboout more loops, nested loops with conditional statements in C Programming language

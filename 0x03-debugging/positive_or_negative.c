6fe27a42589f02132162ecf707a70e9943f7cbf7
Adegbenga Ogundeko <oadegbenga@gmail.com>
2022-07-07 16:55:34 -0700

    Hot fix

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -3,9 +3,11 @@
 /**
 * main - Entry point
 *
+* Description: print the value of i; zero, positive or negative
+*
 * Return: Always 0 (Success)
 */
-int positive_or_negative(int i)
+void positive_or_negative(int i)
 {
 if (i > 0)
 printf("%d is positive\n", i);

a074a72e3f1e273937591aeedb239e3bab8173a1
Adegbenga Ogundeko <oadegbenga@gmail.com>
2022-07-08 08:16:32 -0700

    Task 03: print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print numbers - a function that prints the numebrs, from 0 to 9,
+* followed by a new line.
+* Return: Always 0
+*/
+void print_numbers(void)
+{
+int i = '0';
+for (; i <= '9'; i++)
+_putchar(i);
+_putchar('\n');
+}

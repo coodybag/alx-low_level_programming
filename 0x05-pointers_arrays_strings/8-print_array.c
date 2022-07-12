537e90bc8638e649dc439ca69536ec31bf658134
Adegbenga Ogundeko <oadegbenga@gmail.com>
2022-07-12 02:47:17 -0700

    Task 08: Print the elements of an integer array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_array - function prints the elements of an array of integers.
+*followed by a new line
+*@a: An input array
+*@n: An input integer
+*Return: Nothing
+*/
+void print_array(int *a, int n)
+{
+int i = 0;
+for (; i < n; i++)
+{
+printf("%d", a[i]);
+if (i < n - 1)
+{
+printf(", ");
+}
+}
+putchar('\n');
+}

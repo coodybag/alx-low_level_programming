a1ffff532f542b1353a99a04b6e956a02b3d0995
Adegbenga Ogundeko <oadegbenga@gmail.com>
2022-07-12 02:08:53 -0700

    Task 04: Print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,12 @@
 */
 void print_rev(char *s)
 {
-char *len = s;
-while (*len)
+int len = 0;
+while (s[len] != '\0')
 len++;
-len -= 1;
 while (len)
 {
-_putchar(*s);
-len--;
+_putchar(s[--len]);
 }
 _putchar('\n');
 }

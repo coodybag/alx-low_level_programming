10e651fa0d361af70ab56181ad040bf680147eb7
Adegbenga Ogundeko <oadegbenga@gmail.com>
2022-07-12 01:00:34 -0700

    Task 01: Swapping pointer to integers logic

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*swap_int - function that swaps the value of two integers.
+*@a: an input integer pointer
+*@b: an input integer pointer
+*Return: Nothing
+*/
+void swap_int(int *a, int *b)
+{
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+}
